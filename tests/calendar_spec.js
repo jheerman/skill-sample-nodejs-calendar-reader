3205bfa59aec73d44061bfb3563f6721140245a9
John Heerman <jheerman@gmail.com>
2017-03-17 10:34:12 -0500

    Run test successfully

--- tests/calendar_spec.js
+++ tests/calendar_spec.js
@@ -1,12 +1,6 @@
-import ical from 'ical';
-
 describe("Calendar Suite", function () {
 	it("can run a test successfully", function () {
 		var check = true;
 		expect(check).toBe(true);
-	});
-
-	it("can import iCal library", function() {
-		expect(ical).not.toBe(null);	
 	});
 });
